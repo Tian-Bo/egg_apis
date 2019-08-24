ef8926e10dbdda9f9cf68451c2bd82ec12191295
tianbo <543457309@qq.com>
2019-08-24 10:26:11 +0800

    测试

--- app/controller/wap/users.js
+++ app/controller/wap/users.js
@@ -70,6 +70,11 @@ class UsersController extends Controller {
     const tel = 18580557309
     this.ctx.body = await this.ctx.service.user.find(tel);
   }
+    // 获取用户所有信息
+	async test() {
+		const tel = 18580557309
+		this.ctx.body = await this.ctx.service.user.find(tel);
+	  }
 
 
   // 修改用户信息
