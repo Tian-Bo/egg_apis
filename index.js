d536bb0abf5e31ca31eba9c99798e99928c6cb20
田波 <543457309@qq.com>
2019-08-27 14:19:06 +0800

    测试

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // npm run dev DO NOT read this file
 
 require('egg').startCluster({
-  port: 7001 //process.env.PORT || 7001, // default to 7001
+  port: 443 //process.env.PORT || 7001, // default to 7001
 });
