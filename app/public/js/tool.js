096c0750fab8badfa54aab34a9192a5d4dff086e
田波 <543457309@qq.com>
2019-08-31 20:33:05 +0800

    测试

--- app/public/js/tool.js
+++ app/public/js/tool.js
@@ -4,7 +4,7 @@
  * 生成随机颜色值
  * @returns {String}
  */
-export const toolRandomColor = () => {
+exports.toolRandomColor = () => {
     const rgb = [];
     for (let i = 0; i < 3; ++i) {
         let color = Math.floor(Math.random() * 256).toString(16)
@@ -12,4 +12,12 @@ export const toolRandomColor = () => {
         rgb.push(color)
     }
     return '#' + rgb.join('')
+}
+
+/**
+ * 生成随机四位验证码
+ * @returns {String}
+ */
+exports.toolRandomCode = () => {
+    return Math.random().toString(36).substring(3,7)
 }
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ module.exports = app => {
     // 用户模块
     router.post('/user/login', controller.wap.users.login)
     router.post('/user/register', controller.wap.users.register)
+    router.post('/user/code', controller.wap.users.getCode)
     router.get('/user/info', jwt, controller.wap.users.info)
 
 
